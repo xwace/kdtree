12ffa5182a5d7c5b23ba21a797d7d1155e6d5f2d
xwace <37037519+xwace@users.noreply.github.com>
2023-02-06 15:58:08 +0800

    Update kdTree.py

--- kdTree.py
+++ kdTree.py
@@ -1,7 +1,7 @@
 class KdNode(object):
     def __init__(self, dom_elt, split, left, right):
         self.dom_elt = dom_elt  # k维向量节点(k维空间中的一个样本点)
-        self.split = split  # 整数（进行分割维度的序号）
+        self.split = split  # 0切割x轴,1切割y轴; 整数（进行分割维度的序号）
         self.left = left  # 该结点分割超平面左子空间构成的kd-tree
         self.right = right  # 该结点分割超平面右子空间构成的kd-tree
 
